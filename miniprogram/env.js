be68291037daea055d29acb840efb325a2f8d29d
ASCll <hzxbpld@163.com>
2020-11-04 21:30:17 +0800

    feat: hide js code from codebase

--- miniprogram/env.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.APP_ID = void 0;
-exports.APP_ID = 'wx0611fd1ba2b0bcd6';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZW52LmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiZW52LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFhLFFBQUEsTUFBTSxHQUFHLG9CQUFvQixDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNvbnN0IEFQUF9JRCA9ICd3eDA2MTFmZDFiYTJiMGJjZDYnOyJdfQ==
