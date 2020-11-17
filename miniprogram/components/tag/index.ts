d2c29078669b32787ee795dac57945e1a805657e
ASCll <hzxbpld@163.com>
2020-11-17 23:03:46 +0800

    WIP:

---
+++ miniprogram/components/tag/index.ts
@@ -0,0 +1,22 @@
+// components/tags/tags.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    active: {
+      type: Boolean,
+      value: false,
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {},
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {},
+});
